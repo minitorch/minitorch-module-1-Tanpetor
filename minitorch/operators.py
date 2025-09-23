1033aacd1225159a21fb326247096c4ecaaba3bb
Xipeng Tan <stan_1@edu.hse.ru>
2025-09-23 12:34:45 +0300

    Solve

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -2,41 +2,41 @@ import math
 from typing import Any
 
 
-def mul(a, b) -> Any:
+def mul(a: Any, b: Any) -> Any:
     return a * b
 
 
-def id(a) -> Any:
+def id(a: Any) -> Any:
     return a
 
 
-def add(a, b) -> Any:
+def add(a: Any, b: Any) -> Any:
     return a + b
 
 
-def lt(a, b) -> Any:
+def lt(a: Any, b: Any) -> Any:
     return a < b
 
 
-def eq(a, b) -> Any:
+def eq(a: Any, b: Any) -> Any:
     return a == b
 
 
-def neg(a) -> Any:
+def neg(a: Any) -> Any:
     return -a
 
 
-def max(a, b) -> Any:
+def max(a: Any, b: Any) -> Any:
     if a >= b:
         return a
     return b
 
 
-def is_close(a, b) -> Any:
+def is_close(a: Any, b: Any) -> Any:
     return abs(a - b) < 1e-2
 
 
-def sigmoid(x) -> Any:
+def sigmoid(x: Any) -> Any:
     if x >= 0:
         return 1.0 / (1.0 + math.exp(-x))
     else:
@@ -44,60 +44,60 @@ def sigmoid(x) -> Any:
         return exp_x / (1.0 + exp_x)
 
 
-def relu(x) -> Any:
+def relu(x: Any) -> Any:
     return max(0.0, x)
 
 
-def log(x) -> Any:
+def log(x: Any) -> Any:
     return math.log(x)
 
 
-def exp(x) -> Any:
+def exp(x: Any) -> Any:
     return math.exp(x)
 
 
-def log_back(x, d) -> Any:
+def log_back(x: Any, d: Any) -> Any:
     return d / x
 
 
-def inv(x) -> Any:
+def inv(x: Any) -> Any:
     return 1.0 / x
 
 
-def inv_back(x, d) -> Any:
+def inv_back(x: Any, d: Any) -> Any:
     return -d / (x ** 2.0)
 
 
-def relu_back(x, d) -> Any:
+def relu_back(x: Any, d: Any) -> Any:
     return d if x > 0.0 else 0.0
 
 
-def map(f, lst) -> Any:
+def map(f: Any, lst: Any) -> Any:
     return [f(x) for x in lst]
 
 
-def zipWith(f, lst1, lst2) -> Any:
+def zipWith(f: Any, lst1: Any, lst2: Any) -> Any:
     return [f(x, y) for x, y in zip(lst1, lst2)]
 
 
-def reduce(f, lst, start) -> Any:
+def reduce(f: Any, lst: Any, start: Any) -> Any:
     result = start
     for x in lst:
         result = f(result, x)
     return result
 
 
-def negList(lst) -> Any:
+def negList(lst: Any) -> Any:
     return map(neg, lst)
 
 
-def addLists(lst1, lst2) -> Any:
+def addLists(lst1: Any, lst2: Any) -> Any:
     return zipWith(add, lst1, lst2)
 
 
-def sum_(lst) -> Any:
+def sum_(lst: Any) -> Any:
     return reduce(add, lst, 0.0)
 
 
-def prod(lst) -> Any:
+def prod(lst: Any) -> Any:
     return reduce(mul, lst, 1.0)
