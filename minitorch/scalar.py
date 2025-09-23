beb9a2f2b764da4e48d70611a6b09c881ecd1d6f
Xipeng Tan <stan_1@edu.hse.ru>
2025-09-23 12:50:11 +0300

    Solve

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -163,7 +163,7 @@ class Scalar:
         assert h.last_fn is not None
         assert h.ctx is not None
 
-        local_grads = h.last_fn.backward(h.ctx, d_output)
+        local_grads = h.last_fn._backward(h.ctx, d_output)
 
         if not isinstance(local_grads, (list, tuple)):
             local_grads = [local_grads]
